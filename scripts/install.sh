ce53b4a817145358abed870f3415bf63a82ec337
Sebastian Laskawiec <slaskawi@redhat.com>
2020-11-05 14:37:35 +0100

    Conditional Prometheus CRD installtion
    fixes #153

--- scripts/install.sh
+++ scripts/install.sh
@@ -26,13 +26,14 @@ oc apply -f https://raw.githubusercontent.com/integr8ly/grafana-operator/$GRAFAN
 oc apply -f https://raw.githubusercontent.com/integr8ly/grafana-operator/$GRAFANA_OPERATOR_VERSION/deploy/crds/GrafanaDataSource.yaml
 
 # Prometheus CRDs
-oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/podmonitor.crd.yaml
-oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/prometheus.crd.yaml
-oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/alertmanager.crd.yaml
-oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/prometheusrule.crd.yaml
-oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/servicemonitor.crd.yaml
-
-
+oc get crd | grep prometheus > /dev/null 2>&1
+if [ $? != 0 ]; then
+  oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/podmonitor.crd.yaml
+  oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/prometheus.crd.yaml
+  oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/alertmanager.crd.yaml
+  oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/prometheusrule.crd.yaml
+  oc apply -f https://raw.githubusercontent.com/coreos/prometheus-operator/$PROMETHEUS_OPERATOR_VERSION/example/prometheus-operator-crd/servicemonitor.crd.yaml
+fi
 
 # AMO Deployment
 until oc auth can-i create prometheus -n application-monitoring --as system:serviceaccount:application-monitoring:application-monitoring-operator; do
